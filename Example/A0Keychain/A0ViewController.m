c52468cf518d5672d915a284101e71c54623d9c6
Hernan Zalazar <hernanzalazar@gmail.com>
2014-10-16 11:55:43 -0300

    Example app simple UI.

--- Example/A0Keychain/A0ViewController.m
+++ Example/A0Keychain/A0ViewController.m
@@ -1,12 +1,27 @@
-//
 //  A0ViewController.m
-//  A0Keychain
 //
-//  Created by Hernan Zalazar on 10/15/2014.
-//  Copyright (c) 2014 Hernan Zalazar. All rights reserved.
+// Copyright (c) 2014 Auth0 (http://auth0.com)
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "A0ViewController.h"
+#import <A0Keychain/A0Keychain.h>
 
 @interface A0ViewController ()
 
@@ -14,16 +29,34 @@
 
 @implementation A0ViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)save:(id)sender {
+    NSString *value = self.valueField.text;
+    if (value) {
+        [[A0Keychain keychain] setString:value forKey:@"auth0-keychain-sample"];
+        self.messageLabel.text = [NSString stringWithFormat:@"Saved value '%@' in the Keychain", value];
+    } else {
+        self.messageLabel.text = @"Please enter a value to save in the Keychain";
+    }
+}
+
+- (void)load:(id)sender {
+    NSString *value = [[A0Keychain keychain] stringForKey:@"auth0-keychain-sample"];
+    if (value) {
+        self.messageLabel.text = [NSString stringWithFormat:@"Obtained value '%@' from the Keychain", value];
+    } else {
+        self.messageLabel.text = @"No value found in the keychain";
+    }
+    self.valueField.text = value;
+}
+
+- (void)remove:(id)sender {
+    [[A0Keychain keychain] deleteEntryForKey:@"auth0-keychain-sample"];
+    self.valueField.text = nil;
+    self.messageLabel.text = @"Entry deleted from Keychain";
 }
 
 @end
