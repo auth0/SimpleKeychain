d5fe1c3c2556ea15e993d78c82fe86c4b7d87d89
Hernan Zalazar <hernanzalazar@gmail.com>
2015-10-14 11:05:15 -0300

    Remove Specta/Expecta Start testing with Swift

--- SimpleKeychainTests/A0SimpleKeychainSpec.m
+++ SimpleKeychainTests/A0SimpleKeychainSpec.m
@@ -20,15 +20,15 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-@import Specta;
-@import Expecta;
+@import Quick;
+@import Nimble;
 #import <SimpleKeychain/SimpleKeychain.h>
 
 #define kPublicKeyTag @"public"
 #define kPrivateKeyTag @"private"
 #define kKeychainService @"com.auth0.simplekeychain.tests"
 
-SpecBegin(A0SimpleKeychain)
+QuickSpecBegin(A0SimpleKeychainSpec)
 
 describe(@"A0SimpleKeychain", ^{
 
@@ -38,25 +38,25 @@ describe(@"A0SimpleKeychain", ^{
 
         it(@"should init with default values", ^{
             keychain = [[A0SimpleKeychain alloc] init];
-            expect(keychain.accessGroup).to.beNil();
-            expect(keychain.defaultAccessiblity).to.equal(A0SimpleKeychainItemAccessibleAfterFirstUnlock);
-            expect(keychain.useAccessControl).to.beFalsy();
+            expect(keychain.accessGroup).to(beNil());
+            expect(@(keychain.defaultAccessiblity)).to(equal(@(A0SimpleKeychainItemAccessibleAfterFirstUnlock)));
+            expect(@(keychain.useAccessControl)).to(beFalsy());
         });
 
         it(@"should init with service only", ^{
             keychain = [[A0SimpleKeychain alloc] initWithService:kKeychainService];
-            expect(keychain.accessGroup).to.beNil();
-            expect(keychain.service).to.equal(kKeychainService);
-            expect(keychain.defaultAccessiblity).to.equal(A0SimpleKeychainItemAccessibleAfterFirstUnlock);
-            expect(keychain.useAccessControl).to.beFalsy();
+            expect(keychain.accessGroup).to(beNil());
+            expect(keychain.service).to(equal(kKeychainService));
+            expect(@(keychain.defaultAccessiblity)).to(equal(@(A0SimpleKeychainItemAccessibleAfterFirstUnlock)));
+            expect(@(keychain.useAccessControl)).to(beFalsy());
         });
 
         it(@"should init with service and access group", ^{
             keychain = [[A0SimpleKeychain alloc] initWithService:kKeychainService accessGroup:@"Group"];
-            expect(keychain.accessGroup).to.equal(@"Group");
-            expect(keychain.service).to.equal(kKeychainService);
-            expect(keychain.defaultAccessiblity).to.equal(A0SimpleKeychainItemAccessibleAfterFirstUnlock);
-            expect(keychain.useAccessControl).to.beFalsy();
+            expect(keychain.accessGroup).to(equal(@"Group"));
+            expect(keychain.service).to(equal(kKeychainService));
+            expect(@(keychain.defaultAccessiblity)).to(equal(@(A0SimpleKeychainItemAccessibleAfterFirstUnlock)));
+            expect(@(keychain.useAccessControl)).to(beFalsy());
         });
 
     });
@@ -65,25 +65,25 @@ describe(@"A0SimpleKeychain", ^{
 
         it(@"should create with default values", ^{
             keychain = [A0SimpleKeychain keychain];
-            expect(keychain.accessGroup).to.beNil();
-            expect(keychain.defaultAccessiblity).to.equal(A0SimpleKeychainItemAccessibleAfterFirstUnlock);
-            expect(keychain.useAccessControl).to.beFalsy();
+            expect(keychain.accessGroup).to(beNil());
+            expect(@(keychain.defaultAccessiblity)).to(equal(@(A0SimpleKeychainItemAccessibleAfterFirstUnlock)));
+            expect(@(keychain.useAccessControl)).to(beFalsy());
         });
 
         it(@"should create with service only", ^{
             keychain = [A0SimpleKeychain keychainWithService:kKeychainService];
-            expect(keychain.accessGroup).to.beNil();
-            expect(keychain.service).to.equal(kKeychainService);
-            expect(keychain.defaultAccessiblity).to.equal(A0SimpleKeychainItemAccessibleAfterFirstUnlock);
-            expect(keychain.useAccessControl).to.beFalsy();
+            expect(keychain.accessGroup).to(beNil());
+            expect(keychain.service).to(equal(kKeychainService));
+            expect(@(keychain.defaultAccessiblity)).to(equal(@(A0SimpleKeychainItemAccessibleAfterFirstUnlock)));
+            expect(@(keychain.useAccessControl)).to(beFalsy());
         });
 
         it(@"should create with service and access group", ^{
             keychain = [A0SimpleKeychain keychainWithService:kKeychainService accessGroup:@"Group"];
-            expect(keychain.accessGroup).to.equal(@"Group");
-            expect(keychain.service).to.equal(kKeychainService);
-            expect(keychain.defaultAccessiblity).to.equal(A0SimpleKeychainItemAccessibleAfterFirstUnlock);
-            expect(keychain.useAccessControl).to.beFalsy();
+            expect(keychain.accessGroup).to(equal(@"Group"));
+            expect(keychain.service).to(equal(kKeychainService));
+            expect(@(keychain.defaultAccessiblity)).to(equal(@(A0SimpleKeychainItemAccessibleAfterFirstUnlock)));
+            expect(@(keychain.useAccessControl)).to(beFalsy());
         });
         
     });
@@ -98,29 +98,29 @@ describe(@"A0SimpleKeychain", ^{
         });
 
         it(@"should fail to store a value with nil key", ^{
-            expect([keychain setString:@"value" forKey:nil]).to.beFalsy();
+            expect(@([keychain setString:@"value" forKey:nil])).to(beFalsy());
         });
 
         it(@"should store a a value under a new key", ^{
-            expect([keychain setString:@"value" forKey:key]).to.beTruthy();
+            expect(@([keychain setString:@"value" forKey:key])).to(beTruthy());
         });
 
         it(@"should store a a value under an existing key", ^{
             [keychain setString:@"value1" forKey:key];
-            expect([keychain setString:@"value2" forKey:key]).to.beTruthy();
+            expect(@([keychain setString:@"value2" forKey:key])).to(beTruthy());
         });
 
         it(@"should fail to store a data value with nil key", ^{
-            expect([keychain setData:[NSData data] forKey:nil]).to.beFalsy();
+            expect(@([keychain setData:[NSData data] forKey:nil])).to(beFalsy());
         });
 
         it(@"should store a data value under a new key", ^{
-            expect([keychain setData:[NSData data] forKey:key]).to.beTruthy();
+            expect(@([keychain setData:[NSData data] forKey:key])).to(beTruthy());
         });
 
         it(@"should store a data value under an existing key", ^{
             [keychain setData:[NSData data] forKey:key];
-            expect([keychain setData:[NSData data] forKey:key]).to.beTruthy();
+            expect(@([keychain setData:[NSData data] forKey:key])).to(beTruthy());
         });
 
     });
@@ -136,28 +136,28 @@ describe(@"A0SimpleKeychain", ^{
         });
 
         it(@"should remove a key when value is nil", ^{
-            expect([keychain setString:nil forKey:key]).to.beTruthy();
+            expect(@([keychain setString:nil forKey:key])).to(beTruthy());
         });
 
         it(@"should remove a key when data value is nil", ^{
-            expect([keychain setData:nil forKey:key]).to.beTruthy();
+            expect(@([keychain setData:nil forKey:key])).to(beTruthy());
         });
 
         it(@"should remove entry for key", ^{
-            expect([keychain deleteEntryForKey:key]).to.beTruthy();
+            expect(@([keychain deleteEntryForKey:key])).to(beTruthy());
         });
 
         it(@"should fail with nil key", ^{
-            expect([keychain deleteEntryForKey:nil]).to.beFalsy();
+            expect(@([keychain deleteEntryForKey:nil])).to(beFalsy());
         });
 
         it(@"should fail with nonexisting key", ^{
-            expect([keychain deleteEntryForKey:@"SHOULDNOTEXIST"]).to.beFalsy();
+            expect(@([keychain deleteEntryForKey:@"SHOULDNOTEXIST"])).to(beFalsy());
         });
 
         it(@"should clear all entries", ^{
             [keychain clearAll];
-            expect([keychain stringForKey:key]).to.beNil();
+            expect([keychain stringForKey:key]).to(beNil());
         });
 
     });
@@ -172,40 +172,36 @@ describe(@"A0SimpleKeychain", ^{
             [keychain setString:@"value1" forKey:key];
         });
 
-        it(@"should return that a nonexisting key doesnt exist", ^{
-            expect([keychain stringForKey:@"SHOULDNOTEXIST"]).to.beFalsy();
-        });
-
         it(@"should return that a nil key doesnt exist", ^{
-            expect([keychain stringForKey:nil]).to.beFalsy();
+            expect([keychain stringForKey:nil]).to(beNil());
         });
 
         it(@"should return that a key exists", ^{
-            expect([keychain stringForKey:key]).to.beTruthy();
+            expect([keychain stringForKey:key]).toNot(beNil());
         });
 
         it(@"should return nil string with nil key", ^{
-            expect([keychain stringForKey:nil]).to.beNil();
+            expect([keychain stringForKey:nil]).to(beNil());
         });
 
         it(@"should return nil data with nil key", ^{
-            expect([keychain dataForKey:nil]).to.beNil();
+            expect([keychain dataForKey:nil]).to(beNil());
         });
 
         it(@"should return nil data with non existing key", ^{
-            expect([keychain dataForKey:@"SHOULDNOTEXIST"]).to.beNil();
+            expect([keychain dataForKey:@"SHOULDNOTEXIST"]).to(beNil());
         });
 
         it(@"should return nil string with non existing key", ^{
-            expect([keychain stringForKey:@"SHOULDNOTEXIST"]).to.beNil();
+            expect([keychain stringForKey:@"SHOULDNOTEXIST"]).to(beNil());
         });
 
         it(@"should return string for a key", ^{
-            expect([keychain stringForKey:key]).to.equal(@"value1");
+            expect([keychain stringForKey:key]).to(equal(@"value1"));
         });
 
         it(@"should return data for a key", ^{
-            expect([keychain dataForKey:key]).notTo.beNil();
+            expect([keychain dataForKey:key]).notTo(beNil());
         });
     });
 
@@ -224,31 +220,28 @@ describe(@"A0SimpleKeychain", ^{
             [keychain generateRSAKeyPairWithLength:A0SimpleKeychainRSAKeySize1024Bits
                                       publicKeyTag:kPublicKeyTag
                                      privateKeyTag:kPrivateKeyTag];
-            expect([keychain dataForRSAKeyWithTag:kPublicKeyTag]).notTo.beNil();
-            expect([keychain dataForRSAKeyWithTag:kPrivateKeyTag]).notTo.beNil();
+            expect([keychain dataForRSAKeyWithTag:kPublicKeyTag]).notTo(beNil());
+            expect([keychain dataForRSAKeyWithTag:kPrivateKeyTag]).notTo(beNil());
         });
 
-        sharedExamplesFor(@"failed RSA key generation", ^(NSDictionary *data) {
+        sharedExamples(@"failed RSA key generation", ^(QCKDSLSharedExampleContext context) {
 
             it(@"should fail with NSAssert", ^{
-                expect(^{
+                expectAction(^{
+                    NSDictionary *data = context();
                     [keychain generateRSAKeyPairWithLength:A0SimpleKeychainRSAKeySize1024Bits
                                               publicKeyTag:data[@"publicTag"]
                                              privateKeyTag:data[@"privateTag"]];
-                }).to.raise(NSInternalInconsistencyException);
+                }).to(raiseException());
             });
 
         });
 
-        itShouldBehaveLike(@"failed RSA key generation", @{
-                                                           @"publicTag": kPublicKeyTag,
-                                                           });
+        itBehavesLike(@"failed RSA key generation", ^{ return @{ @"publicTag": kPublicKeyTag }; });
 
-        itShouldBehaveLike(@"failed RSA key generation", @{
-                                                           @"privateTag": kPrivateKeyTag,
-                                                           });
+        itBehavesLike(@"failed RSA key generation", ^{ return @{ @"privateTag": kPrivateKeyTag }; });
 
-        itShouldBehaveLike(@"failed RSA key generation", @{});
+        itBehavesLike(@"failed RSA key generation", ^{ return @{}; });
     });
 
     describe(@"obtain RSA key as NSData", ^{
@@ -266,8 +259,8 @@ describe(@"A0SimpleKeychain", ^{
         });
 
         it(@"should obtain keys", ^{
-            expect([keychain dataForRSAKeyWithTag:kPublicKeyTag]).notTo.beNil();
-            expect([keychain dataForRSAKeyWithTag:kPrivateKeyTag]).notTo.beNil();
+            expect([keychain dataForRSAKeyWithTag:kPublicKeyTag]).notTo(beNil());
+            expect([keychain dataForRSAKeyWithTag:kPrivateKeyTag]).notTo(beNil());
         });
     });
 
@@ -286,16 +279,16 @@ describe(@"A0SimpleKeychain", ^{
         });
 
         it(@"should check if the key exists", ^{
-            expect([keychain hasRSAKeyWithTag:kPublicKeyTag]).to.beTruthy();
-            expect([keychain hasRSAKeyWithTag:kPrivateKeyTag]).to.beTruthy();
+            expect(@([keychain hasRSAKeyWithTag:kPublicKeyTag])).to(beTruthy());
+            expect(@([keychain hasRSAKeyWithTag:kPrivateKeyTag])).to(beTruthy());
         });
 
         it(@"should return NO for nonexisting key", ^{
-            expect([keychain hasRSAKeyWithTag:@"NONEXISTENT"]).to.beFalsy();
+            expect(@([keychain hasRSAKeyWithTag:@"NONEXISTENT"])).to(beFalsy());
         });
     });
 
     
 });
 
-SpecEnd
+QuickSpecEnd
