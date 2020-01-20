6d8b9b33dff59dc0534d1b27b27b7136f2c2a655
Stu <stuart.clift@gwplc.com>
2020-01-20 20:56:44 +0000

    Swift package manager support (#75)
    * Add Package definition
    
    * Update podspec
    
    * Add CI step
    
    * Use enums for versions
    
    * Drop minimum tools requirement to 5.0

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SimpleKeychain",
+    platforms: [
+        .iOS(.v9),
+        .macOS(.v10_11),
+        .watchOS(.v2),
+        .tvOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "SimpleKeychain",
+            targets: ["SimpleKeychain"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Quick/Quick", from: "2.0.0"),
+        .package(url: "https://github.com/Quick/Nimble", from: "8.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "SimpleKeychain",
+            dependencies: [],
+            path: "SimpleKeychain"
+        ),
+        .testTarget(
+            name: "SimpleKeychainTests",
+            dependencies: ["SimpleKeychain", "Quick", "Nimble"],
+            path: "SimpleKeychainTests"
+        ),
+    ]
+)
