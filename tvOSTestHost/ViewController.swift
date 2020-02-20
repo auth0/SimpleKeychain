073aa42d734c586a18080cead02f9817bd1eb3c5
Rita Zerrizuela <zeta@widcket.com>
2020-02-20 20:17:43 -0300

    Fix tvOS tests

---
+++ tvOSTestHost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tvOSTestHost
+//
+//  Created by Rita Zerrizuela on 20/02/2020.
+//  Copyright © 2020 Auth0. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
