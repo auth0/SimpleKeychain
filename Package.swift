b74532ac65f63ddcdf2bb4b89e98593987fa1152
Rita Zerrizuela <zeta@widcket.com>
2020-10-12 17:12:14 -0300

    Update Quick and Nimble

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Quick", from: "2.0.0"),
-        .package(url: "https://github.com/Quick/Nimble", from: "8.0.0"),
+        .package(url: "https://github.com/Quick/Quick", from: "3.0.0"),
+        .package(url: "https://github.com/Quick/Nimble", from: "9.0.0"),
     ],
     targets: [
         .target(
