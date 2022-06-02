b7f6dbf8dcc492636b79d97f2c10fab0ff7cf1e7
Rita Zerrizuela <zeta@widcket.com>
2022-06-01 21:31:34 -0300

    Remove license header from source files [SDK-3389] (#126)
    Remove license header

--- SimpleKeychainApp/AppDelegate.swift
+++ SimpleKeychainApp/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  SimpleKeychainApp
-//
-//  Created by Hernan Zalazar on 8/13/15.
-//  Copyright (c) 2015 Auth0. All rights reserved.
-//
-
 import UIKit
 import SimpleKeychain
 
--- tvOSTestHost/AppDelegate.swift
+++ tvOSTestHost/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  tvOSTestHost
-//
-//  Created by Rita Zerrizuela on 20/02/2020.
-//  Copyright © 2020 Auth0. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
