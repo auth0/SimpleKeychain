d32d6c95c92b64f190df066963c5bf7a262ab21e
Sai Venkat Desu <saivenkatdesu@gmail.com>
2024-06-18 19:59:23 +0530

    feat: added visionOS sample app and linked it as host to visionOS test target

---
+++ visionOSTestHost/visionOSTestHostApp.swift
@@ -0,0 +1,18 @@
+//
+//  visionOSTestHostApp.swift
+//  visionOSTestHost
+//
+//  Created by Desu Sai Venkat on 18/06/24.
+//  Copyright © 2024 Auth0. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct visionOSTestHostApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
