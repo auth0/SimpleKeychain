f75dc740e85922c705c2bce14d1726df60f9f845
Fahad <fahad@2doapp.com>
2025-03-01 13:26:11 +0400

    Cleanup, split into extensions and docs

--- SimpleKeychain/SimpleKeychainObjC.swift
+++ SimpleKeychain/SimpleKeychainObjC.swift
@@ -20,6 +20,16 @@ public typealias SimpleKeychainContext = NSObject
 public class SimpleKeychainObjC: NSObject {
   private let simpleKeychain: SimpleKeychain
   
+  /// Initializes a ``SimpleKeychain`` instance.
+  ///
+  /// - Parameters:
+  ///   - service: Name of the service under which to save items. Defaults to the bundle identifier.
+  ///   - accessGroup: access group for sharing Keychain items. Defaults to `nil`.
+  ///   - accessibility: ``Accessibility`` type the stored items will have. Defaults to ``Accessibility/afterFirstUnlock``.
+  ///   - accessControlFlags: Access control conditions for `kSecAttrAccessControl`.  Defaults to `nil`.
+  ///   - context: `LAContext` used to access Keychain items. Defaults to `nil`.
+  ///   - synchronizable: Whether the items should be synchronized through iCloud. Defaults to `false`.
+  ///   - attributes: Additional attributes to include in every query. Defaults to an empty dictionary.
   @objc public init(service: String,
                     accessGroup: String?,
                     accessibility: SimpleKeychainAccessibility,
@@ -78,7 +88,8 @@ public class SimpleKeychainObjC: NSObject {
     super.init()
   }
   
-  @objc public convenience init(service: String) {
+  /// Convenience initializer that uses the defaults
+  @objc public convenience init(service: String = Bundle.main.bundleIdentifier!) {
     self.init(
       service: service,
       accessGroup: nil,
@@ -89,9 +100,69 @@ public class SimpleKeychainObjC: NSObject {
       attributes: [:]
     )
   }
+}
+
+// MARK: - Retrieve items
+
+public extension SimpleKeychainObjC {
+  /// Retrieves a `String` value from the Keychain.
+  /// 
+  /// ```swift
+  /// var error: NSError?
+  /// let value = try simpleKeychain.string(forKey: "your_key", error: &error)
+  /// ```
+  /// 
+  /// - Parameter key: Key of the Keychain item to retrieve.
+  /// - Parameter error: On failure, will be set to the error that occurred.
+  /// - Returns: The `String` value.
+  @objc func string(forKey key: String, error: NSErrorPointer) -> String? {
+    do {
+      return try simpleKeychain.string(forKey: key)
+    } catch let err {
+      if let error = error {
+        error.pointee = err as NSError
+      }
+      return nil
+    }
+  }
   
-  // Store items - Objective-C style error handling
-  @objc public func setString(_ string: String, forKey key: String, error: NSErrorPointer) -> Bool {
+  /// Retrieves a `Data` value from the Keychain.
+  /// 
+  /// ```swift
+  /// var error: NSError?
+  /// let value = try simpleKeychain.data(forKey: "your_key", error: &error)
+  /// ```
+  /// 
+  /// - Parameter key: Key of the Keychain item to retrieve.
+  /// - Parameter error: On failure, will be set to the error that occurred.
+  /// - Returns: The `Data` value.
+  @objc func data(forKey key: String, error: NSErrorPointer) -> Data? {
+    do {
+      return try simpleKeychain.data(forKey: key)
+    } catch let err {
+      if let error = error {
+        error.pointee = err as NSError
+      }
+      return nil
+    }
+  }
+}
+
+// MARK: - Store items
+
+public extension SimpleKeychainObjC {
+  /// Saves a `String` value with the type `kSecClassGenericPassword` in the Keychain.
+  ///
+  /// ```swift
+  /// var error: NSError?
+  /// try simpleKeychain.setString("some string", forKey: "your_key", error: &error)
+  /// ```
+  ///
+  /// - Parameter string: Value to save in the Keychain.
+  /// - Parameter key: Key for the Keychain item.
+  /// - Parameter error: On failure, will be set to the error that occurred.
+  /// - Returns: `True` on a successful save. When `False`, check error.
+  @objc func setString(_ string: String, forKey key: String, error: NSErrorPointer) -> Bool {
     do {
       try simpleKeychain.set(string, forKey: key)
       return true
@@ -103,7 +174,18 @@ public class SimpleKeychainObjC: NSObject {
     }
   }
   
-  @objc public func setData(_ data: Data, forKey key: String, error: NSErrorPointer) -> Bool {
+  /// Saves a `Data` value with the type `kSecClassGenericPassword` in the Keychain.
+  /// 
+  /// ```swift
+  /// var error: NSError?
+  /// try simpleKeychain.set(data, forKey: "your_key", error: &error)
+  /// ```
+  /// 
+  /// - Parameter data: Value to save in the Keychain.
+  /// - Parameter key: Key for the Keychain item.
+  /// - Parameter error: On failure, will be set to the error that occurred.
+  /// - Returns: `True` on a successful save. When `False`, check error.
+  @objc func setData(_ data: Data, forKey key: String, error: NSErrorPointer) -> Bool {
     do {
       try simpleKeychain.set(data, forKey: key)
       return true
@@ -114,32 +196,23 @@ public class SimpleKeychainObjC: NSObject {
       return false
     }
   }
+}
+
+// MARK: - Delete items
+
+public extension SimpleKeychainObjC {
   
-  // Retrieve items
-  @objc public func string(forKey key: String, error: NSErrorPointer) -> String? {
-    do {
-      return try simpleKeychain.string(forKey: key)
-    } catch let err {
-      if let error = error {
-        error.pointee = err as NSError
-      }
-      return nil
-    }
-  }
-  
-  @objc public func data(forKey key: String, error: NSErrorPointer) -> Data? {
-    do {
-      return try simpleKeychain.data(forKey: key)
-    } catch let err {
-      if let error = error {
-        error.pointee = err as NSError
-      }
-      return nil
-    }
-  }
-  
-  // Delete items
-  @objc public func deleteItem(forKey key: String, error: NSErrorPointer) -> Bool {
+  /// Deletes an item from the Keychain.
+  /// 
+  /// ```swift
+  /// var error: NSError?
+  /// try simpleKeychain.deleteItem(forKey: "your_key", error: &error)
+  /// ```
+  /// 
+  /// - Parameter key: Key of the Keychain item to delete..
+  /// - Parameter error: On failure, will be set to the error that occurred.
+  /// - Returns: `True` on a successful save. When `False`, check error.
+  @objc func deleteItem(forKey key: String, error: NSErrorPointer) -> Bool {
     do {
       try simpleKeychain.deleteItem(forKey: key)
       return true
@@ -151,7 +224,17 @@ public class SimpleKeychainObjC: NSObject {
     }
   }
   
-  @objc public func deleteAll(error: NSErrorPointer) -> Bool {
+  /// Deletes all items from the Keychain for the service and access group values.
+  /// 
+  /// ```swift
+  /// var error: NSError?
+  /// try simpleKeychain.deleteAll(error: &error)
+  /// ```
+  /// 
+  /// - Throws: A ``SimpleKeychainError`` when the SimpleKeychain operation fails.
+  /// - Parameter error: On failure, will be set to the error that occurred.
+  /// - Returns: `True` on a successful save. When `False`, check error.
+  @objc func deleteAll(error: NSErrorPointer) -> Bool {
     do {
       try simpleKeychain.deleteAll()
       return true
@@ -162,9 +245,22 @@ public class SimpleKeychainObjC: NSObject {
       return false
     }
   }
-  
-  // Check if an item exists
-  @objc public func hasItem(forKey key: String, error: NSErrorPointer) -> Bool {
+}
+
+// MARK: - Convenience methods
+
+public extension SimpleKeychainObjC {
+  /// Checks if an item is stored in the Keychain.
+  /// 
+  /// ```swift
+  /// var error: NSError?
+  /// let isStored = try simpleKeychain.hasItem(forKey: "your_key", error: &error)
+  /// ```
+  /// 
+  /// - Parameter key: Key of the Keychain item to check.
+  /// - Parameter error: On failure, will be set to the error that occurred.
+  /// - Returns: Whether the item is stored in the Keychain or not.. When `False`, check error.
+  @objc func hasItem(forKey key: String, error: NSErrorPointer) -> Bool {
     do {
       return try simpleKeychain.hasItem(forKey: key)
     } catch let err {
@@ -175,8 +271,16 @@ public class SimpleKeychainObjC: NSObject {
     }
   }
   
-  // Get all keys
-  @objc public func keys(error: NSErrorPointer) -> [String]? {
+  /// Retrieves the keys of all the items stored in the Keychain for the service and access group values.
+  /// 
+  /// ```swift
+  /// var error: NSError?
+  /// let keys = try simpleKeychain.keys(error: &error)
+  /// ```
+  /// 
+  /// - Returns: A `String` array containing the keys.
+  /// - Parameter error: On failure, will be set to the error that occurred.
+  @objc func keys(error: NSErrorPointer) -> [String]? {
     do {
       return try simpleKeychain.keys()
     } catch let err {
