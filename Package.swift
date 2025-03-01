3c20ebe5e38138b7e51849f68efa60bc7e3dcf50
Fahad <fahad@2doapp.com>
2025-03-01 17:08:24 +0400

    Support macOS 10.15

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "SimpleKeychain",
-    platforms: [.iOS(.v14), .macOS(.v11), .tvOS(.v14), .watchOS(.v7), .visionOS(.v1)],
+    platforms: [.iOS(.v14), .macOS(.v10_15), .tvOS(.v14), .watchOS(.v7), .visionOS(.v1)],
     products: [
       .library(name: "SimpleKeychain", targets: ["SimpleKeychain"]),
       .library(name: "SimpleKeychain-dynamic", type: .dynamic, targets: ["SimpleKeychain"])
